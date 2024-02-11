1fd97e59bd044fa1f4dd04bcf263b46e60d29200
Marcos <104943037+MarcosSSA011@users.noreply.github.com>
2024-02-10 21:15:13 -0600

    m3 t5 1

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,17 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+    	
+    	int divisor = 5;
+    	
+    	float result = toBeDivided /divisor;
+    	
+    	System.out.println(result);
+    }
+    
+    public static void main(String[] args) {
+        NumberDivider numDivider = new NumberDivider();        
+        int toBeDivided = 30;
+        numDivider.divide(toBeDivided);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,17 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
-    public void convert(float minutes) {
+    public void convert(float minutes) {    	
+    	 float seconds = minutes * 60;    
+    	 
+    	 System.out.println(seconds);
     }
+    
+    public static void main(String[] args) {
+        TimeConvertor convertor = new TimeConvertor();        
+        float minutes = 3;
+        convertor.convert(minutes);
+    }
+    
+  
 }
